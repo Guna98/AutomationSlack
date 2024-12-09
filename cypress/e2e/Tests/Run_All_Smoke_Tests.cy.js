f72054b985cdf1086b98237279109c85453f60a1
Guna98 <gunakoushik1808@gmail.com>
2024-12-09 13:14:07 +0530

    Change

--- cypress/e2e/Tests/Run_All_Smoke_Tests.cy.js
+++ cypress/e2e/Tests/Run_All_Smoke_Tests.cy.js
@@ -4,29 +4,29 @@
 
 import './Login/Login.cy.js';
 
-import './PageSetup/AddMaterial.cy.js';
-import './PageSetup/preferences.cy.js';
-import './UserManagement/AddAuditor.cy.js';
+// import './PageSetup/AddMaterial.cy.js';
+// import './PageSetup/preferences.cy.js';
+// import './UserManagement/AddAuditor.cy.js';
 
-import './Reports/AddBRSRReport.cy.js';
-import './Reports/CustomReport.cy.js';
-import './Reports/GRIReport.cy.js';
+// import './Reports/AddBRSRReport.cy.js';
+// import './Reports/CustomReport.cy.js';
+// import './Reports/GRIReport.cy.js';
 
-import './UserManagement/AddSupplier.cy.js';
-import './Mailinator/Mailinator_supplier.cy.js';
-import './UserManagement/BusinessUnits.cy.js';
-import './Mailinator/Mailinator_bu.cy.js';
-import './UserManagement/AddCollab.cy.js';
-import './Mailinator/Mailinator_collab.cy.js';
-// import './UserManagement/AddReviewer.cy.js';
-// import './Mailinator/Mailinator_reviewer.cy.js';
-// import './UserManagement/AddSTMember.cy.js';
-import './Mailinator/Mailinator_stmember.cy.js';
+// import './UserManagement/AddSupplier.cy.js';
+// import './Mailinator/Mailinator_supplier.cy.js';
+// import './UserManagement/BusinessUnits.cy.js';
+// import './Mailinator/Mailinator_bu.cy.js';
+// import './UserManagement/AddCollab.cy.js';
+// import './Mailinator/Mailinator_collab.cy.js';
+// // import './UserManagement/AddReviewer.cy.js';
+// // import './Mailinator/Mailinator_reviewer.cy.js';
+// // import './UserManagement/AddSTMember.cy.js';
+// import './Mailinator/Mailinator_stmember.cy.js';
 
-import './AssignQuestionsCollab/AssignAnnualQuestionsCollab.cy.js';
-import './AssignQuestionsCollab/AssignMonthlyQuestionsCollab.cy.js';
-import './AssignQuestionsBU/AssignAnnualQuestionsBU.cy.js';
-import './AssignQuestionsBU/AssignMonthlyQuestionsBU.cy.js';
+// import './AssignQuestionsCollab/AssignAnnualQuestionsCollab.cy.js';
+// import './AssignQuestionsCollab/AssignMonthlyQuestionsCollab.cy.js';
+// import './AssignQuestionsBU/AssignAnnualQuestionsBU.cy.js';
+// import './AssignQuestionsBU/AssignMonthlyQuestionsBU.cy.js';
 
-import './UserManagement/SupplierBulkUpload.cy.js';
+// import './UserManagement/SupplierBulkUpload.cy.js';
 
